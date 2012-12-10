dd57dd82f0777cc06dfcc1854a661d5f20832962
Thiago Pontes <tpontes@officedrop.com>
2012-12-10 15:59:24 -0300

    tab/space style related fixes

--- js/app/models/recipe.js
+++ js/app/models/recipe.js
@@ -4,31 +4,31 @@ define(function(require){
 
 	var Recipe = Backbone.Model.extend({
 
-	    validate: function(attrs) {            
+        validate: function(attrs) {
 			if(!this._isValidName(attrs.name))
-                return "Nome inválido!";                        
+                return "Nome inválido!";
             else if(attrs.name.trim().length < 3 || attrs.name.trim().length > 30)
-                return "A quantidade de caracteres devem ser entre 3 e 30";                      
+                return "A quantidade de caracteres devem ser entre 3 e 30";
             else if(!this._isValidPreparation(attrs.preparation))
-            	return "Dados incorretos!";                      
+                return "Dados incorretos!";
 			else if(attrs.preparation.trim().length < 10 || attrs.preparation.trim().length > 2000)
-                return "A quantidade de caracteres devem ser entre 10 e 2000";                    	
+                return "A quantidade de caracteres devem ser entre 10 e 2000";
             else if(!this._isValidDescription(attrs.description))
-            	return "Dados incorretos!";                      
+                return "Dados incorretos!";
 			else if(attrs.description.trim().length < 10 || attrs.description.trim().length > 1000)
-                return "A quantidade de caracteres devem ser entre 10 e 1000";                    	
- 		},
+                return "A quantidade de caracteres devem ser entre 10 e 1000";
+        },
 
         _isValidName: function(name) {
-        	return name;
+            return name;
         },
 
         _isValidPreparation: function(preparation) {
-        	return preparation;
+            return preparation;
         },
 
         _isValidDescription: function(description) {
-        	return description;
+            return description;
         }
 
 	});
